99436d3742fa3ca3bd6dc540104ca73728b3b274
EVK <eric@ericvankirk.com>
2022-07-06 21:50:20 -0500

    api update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,24 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+    
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
 };
-getUserRepos();
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+// getUserRepos("microsoft");
+
